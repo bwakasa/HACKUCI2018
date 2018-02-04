4d0f5ec4aa8e0c4c7578b09a94ef195bdcf9881c
lukewill18 <lukewill18@gmail.com>
2018-02-04 06:59:07 -0800

    Lot of changes (read description)
    -Data from database is now transferred to main page via hash table
    -Scores update in firebase when you vote on pictures now
    -Voting on a picture makes the next set of pictures appear

--- Test3/app/src/main/java/com/example/bwakasa/uci/edu/test3/UploadScreen.java
+++ Test3/app/src/main/java/com/example/bwakasa/uci/edu/test3/UploadScreen.java
@@ -8,19 +8,29 @@ import android.os.Bundle;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class UploadScreen extends AppCompatActivity {
+
+public class UploadScreen extends AppCompatActivity implements Serializable{
 
     Button yourPet;
     Button theirPet;
@@ -45,20 +55,38 @@ public class UploadScreen extends AppCompatActivity {
 
     private String key;
 
+    private static final String TAG = "Testing: ";
+
+    private FirebaseAuth mAuth;
+
+    private HashMap<String,HashMap<String,String>> table;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_screen);
         Submit = (Button) findViewById(R.id.submit);
         yourPet = (Button) findViewById(R.id.yourpetPic);
         theirPet = (Button) findViewById(R.id.theirpetPic);
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Matches");
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
 
         mStorage = FirebaseStorage.getInstance().getReference();
 
-        key = mDatabase.child("Matches").push().getKey();
+        key = mDatabase.child("Matches").push().getKey(); // .child("Matches")
+        //Log.i("key_is", key);
         picked_yours = false;
         picked_theirs = false;
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                table = (HashMap<String, HashMap<String,String>>) snapshot.getValue();
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.i("Cancelled: ", "Failed to read value.");
+            }
+        });
 
         yourPet.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,13 +115,15 @@ public class UploadScreen extends AppCompatActivity {
                 if (picked_yours && picked_theirs) {
                     yourPic.putFile(your_uri);
                     theirPic.putFile(their_uri);
-
-                    mDatabase.child(key).child("image_key1").setValue(your_uri.getLastPathSegment());
-                    mDatabase.child(key).child("image_key2").setValue(their_uri.getLastPathSegment());
-                    mDatabase.child(key).child("score1").setValue(0);
-                    mDatabase.child(key).child("score2").setValue(0);
-                    Toast myToast = Toast.makeText(getApplicationContext(), "Submitted!", Toast.LENGTH_LONG);
-                    myToast.show();
+                    if(mAuth != null) {
+                        mDatabase.child(key).child("image_key1").setValue(your_uri.getLastPathSegment());
+                        mDatabase.child(key).child("image_key2").setValue(their_uri.getLastPathSegment());
+                        mDatabase.child(key).child("score1").setValue("0");
+                        mDatabase.child(key).child("score2").setValue("0");
+                        Intent myIntent = new Intent(v.getContext(), firstPage.class);
+                        myIntent.putExtra("table", table);
+                        startActivity(myIntent);
+                    }
                 }
                 else {
                     Toast myToast = Toast.makeText(getApplicationContext(), "Please upload both pictures first.", Toast.LENGTH_LONG);
@@ -123,3 +153,4 @@ public class UploadScreen extends AppCompatActivity {
         }
     }
 }
+
--- Test3/app/src/main/java/com/example/bwakasa/uci/edu/test3/firstPage.java
+++ Test3/app/src/main/java/com/example/bwakasa/uci/edu/test3/firstPage.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextClock;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
@@ -17,14 +20,16 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Set;
+import java.util.Iterator;
 
-public class firstPage extends AppCompatActivity implements View.OnClickListener{
+public class firstPage extends AppCompatActivity implements View.OnClickListener, Serializable {
 
     ImageButton b1;
     ImageButton b2;
@@ -36,11 +41,23 @@ public class firstPage extends AppCompatActivity implements View.OnClickListener
     int counter;
     int counter2;
     String strcounter;
-    Set<String> sSet;
+    String first_img_loc;
+    String second_img_loc;
 
     FirebaseDatabase FDB;
     DatabaseReference DBref;
     DatabaseReference DBref2;
+
+    StorageReference storageRef;
+
+    private HashMap<String,HashMap<String,String>> table;
+
+    private HashMap.Entry<String, HashMap<String,String>> current_match;
+
+    static Iterator it;
+
+    ImageButton image1;
+    ImageButton image2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +65,45 @@ public class firstPage extends AppCompatActivity implements View.OnClickListener
         setContentView(R.layout.activity_first_page);
         setTitle("PetCompare");
         FDB = FirebaseDatabase.getInstance();
-        DBref = FDB.getReference("image_key1");
-        DBref2 = FDB.getReference("image_key2");
+        //  DBref = FDB.getReference("image_key1");
+        // DBref2 = FDB.getReference("image_key2");
+        DBref = FDB.getReference();
 
+        storageRef = FirebaseStorage.getInstance().getReference().child("Photos");
+
+        table = (HashMap<String,HashMap<String,String>>) getIntent().getSerializableExtra("table");
+
+
+        first_img_loc = "1608253266";
+        second_img_loc = "1987731159";
+
+        if(table != null)
+        {
+            //   Log.i("table: ", table.toString());
+            //  Log.i("First item: ", table.get("-L4VYSamY8qmoyGBo-fu").get("image_key1"));
+            it = table.entrySet().iterator();
+            current_match = (HashMap.Entry<String, HashMap<String,String>>) it.next();
+            first_img_loc = current_match.getValue().get("image_key1");
+            second_img_loc = current_match.getValue().get("image_key2");
+        }
+
+        storageRef = storageRef.child(first_img_loc);
+
+        image1 = (ImageButton) findViewById(R.id.imageB1);
+        image2 = (ImageButton) findViewById(R.id.imageB2);
+        Glide.with(this /* context */)
+                .using(new FirebaseImageLoader())
+                .load(storageRef)
+                .into(image1);
+
+        storageRef = storageRef.getParent().child(second_img_loc);
+
+        Glide.with(this /* context */)
+                .using(new FirebaseImageLoader())
+                .load(storageRef)
+                .into(image2);
+
+        storageRef = storageRef.getParent();
 
         b1 = findViewById(R.id.imageB1);
         b2 = findViewById(R.id.imageB2);
@@ -66,55 +119,74 @@ public class firstPage extends AppCompatActivity implements View.OnClickListener
         b1.setOnClickListener(this);
         b2.setOnClickListener(this);
 
-        DBref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for( DataSnapshot sShot:dataSnapshot.child("Match").getChildren()){
-                   String s = (sShot.child("image_key1").getValue()).toString();
-                   
-                }
-
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-        {
-
-        }
-
-
     }
 
+    public void display(HashMap.Entry<String, HashMap<String,String>> match) {
+        String first_image_loc = match.getValue().get("image_key1");
+        String second_image_loc = match.getValue().get("image_key2");
+
+        b1.setVisibility(View.VISIBLE);
+        b2.setVisibility(View.VISIBLE);
+        storageRef = storageRef.child(first_image_loc);
+        Glide.with(this /* context */)
+                .using(new FirebaseImageLoader())
+                .load(storageRef)
+                .into(image1);
+
+        storageRef = storageRef.getParent().child(second_image_loc);
+        Glide.with(this /* context */)
+                .using(new FirebaseImageLoader())
+                .load(storageRef)
+                .into(image2);
+        storageRef = storageRef.getParent();
+    }
 
     public void onClick(View view) {
 
         if (view == b1)
         {
-            counter++;
-            strcounter = Integer.toString(counter);
-            score.setText(strcounter);
+            String key = "-L4V-0TAYMg3jQ1dLXdj";
+            if(current_match != null) {
+                key = current_match.getKey();
+            }
+            // Log.i("key is: ", key);
+            String score1_str = current_match.getValue().get("score1");
+            int score_1 = Integer.parseInt(score1_str);
+            score_1 += 1;
+            //give score1 back to cloud
+
+            DBref.child(key).child("score1").setValue(Integer.toString(score_1));
+
             b1.setVisibility(View.GONE);
             b2.setVisibility(View.GONE);
+            current_match = (HashMap.Entry<String, HashMap<String,String>>) it.next();
+            display(current_match);
         }
         else if (view == b2)
         {
-            counter2++;
-            strcounter = Integer.toString(counter2);
-            score2.setText(strcounter);
+            String key = "-L4V-0TAYMg3jQ1dLXdj";
+            if(current_match != null) {
+                key = current_match.getKey();
+            }
+            // Log.i("key is: ", key);
+            String score1_str = current_match.getValue().get("score2");
+            int score_2 = Integer.parseInt(score1_str);
+            score_2 += 1;
+            //give score1 back to cloud
+
+            DBref.child(key).child("score2").setValue(Integer.toString(score_2));
+
             b1.setVisibility(View.GONE);
             b2.setVisibility(View.GONE);
+            current_match = (HashMap.Entry<String, HashMap<String,String>>) it.next();
+            display(current_match);
         }
 
         else if(view == rate)
         {
 //            rate.setVisibility(View.GONE);
 
-       //     myIntent.putExtra(VARIABLE NAME, INFO GOES HERE); <- if we need to pass in variables
+            //     myIntent.putExtra(VARIABLE NAME, INFO GOES HERE); <- if we need to pass in variables
 
         }
         else if(view == compete)
@@ -129,4 +201,7 @@ public class firstPage extends AppCompatActivity implements View.OnClickListener
         }
     }
 
+
 }
+
+
